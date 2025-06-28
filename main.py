7caeb691a4b9b46c87c6c61f27ffa9b851c19568
hdavidkim4326 <hdavidkim4326@gmail.com>
2025-06-28 12:50:34 +0900

    Update conflict solve main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 
 import cart
 
-def main():
-	return cart.cart()
+
 
 import hate
 
-def main():
-	return hate.hate()
+
 
 import like
 
 def main():
-	return like.like()
+	return like.like()+hate.hate()+cart.cart()
 
